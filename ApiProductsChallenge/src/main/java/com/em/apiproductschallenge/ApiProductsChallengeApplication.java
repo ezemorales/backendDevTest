0b4e9683843fb98779ed06a961f7885154f45e56
Ezequiel Morales <ezequiel.morales@lamercantil.com.ar>
2022-11-21 11:35:15 -0300

    New project

---
+++ ApiProductsChallenge/src/main/java/com/em/apiproductschallenge/ApiProductsChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.em.apiproductschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductsChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProductsChallengeApplication.class, args);
+	}
+
+}
---
+++ ApiProductsChallenge/src/test/java/com/em/apiproductschallenge/ApiProductsChallengeApplicationTests.java
@@ -0,0 +1,9 @@
+package com.em.apiproductschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductsChallengeApplicationTests {
+
+}
